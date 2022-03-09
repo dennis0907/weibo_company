6eb4a6647bec80985c0744642e53fcfaa2064ce0
dennis <dennis.know.why@gmail.com>
2022-03-09 07:15:06 +0000

    屬性初始化

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,6 +2,20 @@
 <html>
 <head>
     <title>@yield('title','Weibo App') -> laravel入門 </title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
-    @yield('content')
+<body>
+    <nav class="navbar navar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav">
+              <li class="nav-item"><a class="nav-link" href="/help">幫助</a></li>
+              <li class="nav-item" ><a class="nav-link" href="#">登入</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts/default')
 @section('content')
-    <h1>主頁</h1>
+    <div class="mt-4 p-5 bg-light rounded">
+        <h1>Hello Laravel</h1>
+        <p class="lead" >
+            您現在看到的是<a href="#">Laravel 入門</a>的範例主頁。
+        </p>
+        <p>
+            從這開始練習。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#">現在註冊</a>
+        </p>
+    </div>
 @stop
