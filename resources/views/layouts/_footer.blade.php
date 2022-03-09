69363d04af35544947a6a1d7ae257cf3147b744f
dennis <dennis.know.why@gmail.com>
2022-03-09 08:07:10 +0000

    頁首頁尾

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="#">
+    <a href="#" target="_blank">@ this web make from Dennis</a>
+    <div class="float-end">
+        <a href="/about">關於</a>
+    </div>
+</footer>
