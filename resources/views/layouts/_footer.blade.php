1c66818a13b9e99280dd21593b8764c23a989d8d
dennis <dennis.know.why@gmail.com>
2022-03-10 03:09:38 +0000

    model

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="#">
-    <a href="#" target="_blank">@ this web make from Dennis</a>
+    <a href="#" target="_blank">This web make from Dennis</a>
     <div class="float-end">
     <a href="{{route('about')}}">關於</a>
     </div>
