2155fd0b5690ec5132bb0de46455b36cf9809e18
dennis <dennis.know.why@gmail.com>
2022-03-12 09:25:17 +0000

    登入審核

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    public function create() {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request) {
+
+        $credentials = $this->validate($request,[
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success','歡迎回來!');
+            return redirect()->route('users.show',[Auth::user()]);
+        } else {
+            session()->flash('danger','抱歉您的郵件或密碼有誤');
+            return redirect()->back()->withInput();
+        }
+        return;
+    }
+}
